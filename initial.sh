d6bc936f63e76cee5cf3a4942696a98c5f7496ce
Flaky <407440485@gms.tku.edu.tw>
2023-03-30 01:27:35 +0800

    modified initial.sh

--- initial.sh
+++ initial.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 sudo apt-get install libmysqlclient-dev
 pipenv install 
-cd project_test/backend
-sudo docker run -itd --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234 mysql 
+sudo docker run -itd --name mysql -v ~/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234 mysql 
 sudo docker run --name phpmyadmin -d --link mysql -e PMA_HOST=mysql -p 8080:80 phpmyadmin/phpmyadmin
 
 # pipenv run python manage.py makemigrations
