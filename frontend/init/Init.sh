96b606340e7b13ca9a4ba3270597efa2d959337c
Flaky <407440485@gms.tku.edu.tw>
2023-04-12 21:42:55 +0800

    test

--- frontend/init/Init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo -e '\nInit.sh Begin\n';
-
-sh /init/InstallModule.sh;
-sh /init/RunServer.sh;
-
-echo -e '\nInit.sh Success\n';
--- frontend/main/src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducer/CombinReducer';
-import thunk from 'redux-thunk';
-const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-);
-
-export default store;
