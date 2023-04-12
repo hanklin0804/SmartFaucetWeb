3ce8c566e3f4379fd4a307b84352fbcba239f35b
Flaky <407440485@gms.tku.edu.tw>
2023-04-12 20:46:59 +0800

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'BLACKLIST_AFTER_ROTATION': True,
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
 } 
 
 # JWT_AUTH = {
