09b9da990e96373550c4c75fc6a3bb18c318a864
hanklin0804 <75530989+hanklin0804@users.noreply.github.com>
2023-04-17 13:25:24 +0800

    Update initial.sh

--- commandScript/initial.sh
+++ commandScript/initial.sh
@@ -37,7 +37,7 @@ sudo usermod -aG docker $USER
 sudo reboot
 
 # -------安裝docker compose--------
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
 docker-compose --version
 
