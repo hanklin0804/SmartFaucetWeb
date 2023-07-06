24b7c7deb70255a2a8ed295d9ee04258180c8905
Woody <woody38921218@gmail.com>
2023-07-06 13:00:23 -0700

    homepage in process _ver1

--- app/home/components/card.js
+++ app/home/components/card.js
@@ -7,20 +7,77 @@ import { CardActionArea } from '@mui/material';
 import Image from 'next/image'
 export default function ActionAreaCard({ title, content, image }) {
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-
-                <Image
-                    src={image}
-                    width={250}
-                    height={250}
-                    alt="Picture of the author"
+        <Card sx={{
+            maxWidth: 345,
+            mt:
+            {
+                lg: 15,
+                md: 14,
+                sm: 10,
+                xs: 5,
+            },
+            ml:
+            {
+                lg: 3,
+                md: 5,
+                sm: 5,
+                xs: 8,
+            }
+        }}>
+            <CardActionArea sx={{
+                width:
+                {
+                    lg: 300,
+                    md: 200,
+                    sm: 175,
+                    xs: 200,
+                }
+            }}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={image}
+                    sx={{
+                        width:
+                        {
+                            lg: 300,
+                            md: 200,
+                            sm: 150,
+                            xs: 200,
+                        },
+                        height:
+                        {
+                            lg: 300,
+                            md: 200,
+                            sm: 150,
+                            xs: 200,
+                        },
+                    }}
                 />
-                {/* <>{image}</> */}
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography gutterBottom component="div"
+                        sx={{
+                            mt: -4,
+                            fontSize:
+                            {
+                                lg: 30,
+                                md: 20,
+                                sm: 15,
+                                xs: 20,
+                            },
+                            fontWeight: 700
+
+                        }}>
                         {title}
                     </Typography>
+                    <div
+                        style={{
+                            width: '100%',
+                            height: '1px',
+                            background: 'black',
+                            margin: '8px 0',
+                        }}
+                    ></div>
                     <Typography variant="body2" color="text.secondary">
                         {content}
                     </Typography>
